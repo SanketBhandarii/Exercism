23118c8365b5c923f734f68c1765b6429d06ba49
SanketBhandarii <bhandarisanketp@gmail.com>
2025-04-21 12:31:45 +0530

    74-strain

---
+++ Typescript/74-strain.ts
@@ -0,0 +1,22 @@
+export function keep<T>(collection: T[], predicate: (item: T) => boolean): T[] {
+  const result: T[] = [];
+  for (const item of collection) {
+    if (predicate(item)) {
+      result.push(item);
+    }
+  }
+  return result;
+}
+
+export function discard<T>(
+  collection: T[],
+  predicate: (item: T) => boolean
+): T[] {
+  const result: T[] = [];
+  for (const item of collection) {
+    if (!predicate(item)) {
+      result.push(item);
+    }
+  }
+  return result;
+}
